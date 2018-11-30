72bbd0ec9c1e2c64b78db92f9749a50c6d70074c
frenetic <frenetic@ubuntu-1404.vagrantup.com>
2018-11-30 16:44:17 +0000

    se mueve el archivo automaticamente

---
+++ copyFiles.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo cp /home/frenetic/Desktop/enrejado/zgn-fattree.py /home/frenetic/mininet/custom/
+echo "zgn-fattree copiado"
+sudo cp /home/frenetic/Desktop/enrejado/zgn_fattree_controller.py /home/frenetic/pox/pox/samples/
+echo "zaragoza_lswitch_fattree copiado"
